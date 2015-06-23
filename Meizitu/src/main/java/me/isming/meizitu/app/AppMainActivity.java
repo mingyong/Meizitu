0c918d28d8ecd2482acad3eeb2cb2959404b4059
scola <shaozheng.wu@gmail.com>
2015-06-23 17:02:58 +0800

    add feed menu and save in sharedprefrerence

--- Meizitu/src/main/java/me/isming/meizitu/app/AppMainActivity.java
+++ Meizitu/src/main/java/me/isming/meizitu/app/AppMainActivity.java
@@ -1,16 +1,31 @@
 package me.isming.meizitu.app;
 
 import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.FragmentManager;
 import android.os.Bundle;
+import android.util.Patterns;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.support.v4.widget.DrawerLayout;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Toast;
 
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import com.umeng.analytics.MobclickAgent;
 import com.umeng.fb.FeedbackAgent;
 import com.umeng.update.UmengUpdateAgent;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.List;
+import java.util.Map;
 
 import me.isming.meizitu.util.CLog;
 import me.isming.meizitu.util.DeviceUtil;
@@ -22,11 +37,15 @@ public class AppMainActivity extends BaseActivity
     /**
      * Fragment managing the behaviors, interactions and presentation of the navigation drawer.
      */
+    private static final String PREF_FEED_NAME = "allFeeds";
+
     private NavigationDrawerFragment mNavigationDrawerFragment;
 
     private BaseFragment mContentFragment;
 
     private MenuItem mMenu;
+
+    private Map<String, String> mFeeds = new HashMap<String, String>();
 
     /**
      * Used to store the last screen title. For use in {@link #restoreActionBar()}.
@@ -41,6 +60,14 @@ public class AppMainActivity extends BaseActivity
         mNavigationDrawerFragment = (NavigationDrawerFragment)
                 getSupportFragmentManager().findFragmentById(R.id.navigation_drawer);
         mTitle = getTitle();
+        SharedPreferences feeds = getPreferences(Context.MODE_PRIVATE);
+        String json_feeds = feeds.getString(PREF_FEED_NAME, null);
+        if (json_feeds == null) {
+            mFeeds.put("haixiu", "http://23.252.109.110:5000/results/dump/haixiuzu2.txt");
+        } else {
+            mFeeds = new Gson().fromJson(json_feeds, new TypeToken<Map<String, String>>() {
+            }.getType());
+        }
 
         // Set up the drawer.
         mNavigationDrawerFragment.setUp(
@@ -50,6 +77,15 @@ public class AppMainActivity extends BaseActivity
         MobclickAgent.updateOnlineConfig(this);
         UmengUpdateAgent.update(this);
         CLog.i(DeviceUtil.getDeviceInfo(this));
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        SharedPreferences feeds = getPreferences(Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = feeds.edit();
+        editor.putString(PREF_FEED_NAME, new Gson().toJson(mFeeds));
+        editor.commit();
     }
 
     @Override
@@ -132,6 +168,42 @@ public class AppMainActivity extends BaseActivity
             if (mContentFragment != null) {
                 mContentFragment.scrollTopAndRefresh();
             }
+        } else if(id == R.id.action_add) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle(getString(R.string.action_add));
+            LinearLayout layout = new LinearLayout(this);
+            layout.setOrientation(LinearLayout.VERTICAL);
+
+            final EditText titleBox = new EditText(this);
+            titleBox.setHint("Title");
+            layout.addView(titleBox);
+
+            final EditText descriptionBox = new EditText(this);
+            descriptionBox.setHint("URL");
+            layout.addView(descriptionBox);
+
+            builder.setView(layout);
+
+            builder.setPositiveButton(getString(android.R.string.ok),
+                    new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            // get user input and set it to result
+                            // edit text
+//                            result.setText(userInput.getText());
+                            CLog.d(titleBox.getText());
+                            if (titleBox.getText().toString().trim() != "" && Patterns.WEB_URL.matcher(descriptionBox.getText().toString().trim()).matches()) {
+                                mFeeds.put(titleBox.getText().toString().trim(), descriptionBox.getText().toString().trim());
+                            }
+//                            Toast.makeText(AppMainActivity.this, "Get URL", Toast.LENGTH_LONG);
+                        }
+                    })
+                    .setNegativeButton(getString(android.R.string.cancel),
+                            new DialogInterface.OnClickListener() {
+                                public void onClick(DialogInterface dialog, int id) {
+                                    dialog.cancel();
+                                }
+                            });
+            builder.create().show();
         }
         return super.onOptionsItemSelected(item);
     }
